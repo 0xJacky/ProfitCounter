b7b5a8f370d6ff79534de22c8042a738eb3e62c2
Jacky <jacky-943572677@qq.com>
2020-12-31 01:19:21 +0800

    Update background.js

--- src/background.js
+++ src/background.js
@@ -16,7 +16,7 @@ protocol.registerSchemesAsPrivileged([
 async function createWindow() {
     // Create the browser window.
     const win = new BrowserWindow({
-        width: 1600,
+        width: 1700,
         height: 700,
         //resizable: false,
         webPreferences: {
